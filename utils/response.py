1cdc738c9577aa354f9b836dc3deb6f41b24a58b
Fabricio Coelho <fabricio.coelho@stoodi.com.br>
2020-02-10 22:30:13 -0300

    fix the hard coded status

--- utils/response.py
+++ utils/response.py
@@ -4,6 +4,6 @@ import json
 def make_response(body, status_code, headers={}):
     return {
         'body': json.dumps(body),
-        'statusCode': 200,
+        'statusCode': status_code,
         'headers': headers
     }
